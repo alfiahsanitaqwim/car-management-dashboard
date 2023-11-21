29730b8211e5b96a810d1fb87c5d4e1dec296999
alfiahsanitaqwim <127421190+alfiahsanitaqwim@users.noreply.github.com>
2023-11-21 17:10:25 +0700

    initial commit

--- seeds/car_types.ts
+++
@@ -1,7 +0,0 @@
-const carTypes = [
-    { id: 1, name: "Sedan" },
-    { id: 2, name: "SUV" },
-    { id: 3, name: "Convertible" },
-    { id: 4, name: "Hatchback" },
-    { id: 5, name: "Minivan" }
-]
