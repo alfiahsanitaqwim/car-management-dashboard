8365f17b490b10e71391fca64a75daf94c546df0
alfiahsanitaqwim <127421190+alfiahsanitaqwim@users.noreply.github.com>
2023-11-22 15:31:29 +0700

    delete trash and fix service

--- knexfile.ts
+++ knexfile.ts
@@ -2,11 +2,56 @@ import type { Knex } from "knex";
 
 // Update with your config settings.
 
-const config: { [key: string]: Knex.Config } = {
+// const config: { [key: string]: Knex.Config } = {
+//   development: {
+//     client: "sqlite3",
+//     connection: {
+//       filename: "./dev.sqlite3"
+//     }
+//   },
+
+//   staging: {
+//     client: "postgresql",
+//     connection: {
+//       database: "my_db",
+//       user: "username",
+//       password: "password"
+//     },
+//     pool: {
+//       min: 2,
+//       max: 10
+//     },
+//     migrations: {
+//       tableName: "knex_migrations"
+//     }
+//   },
+
+//   production: {
+//     client: "postgresql",
+//     connection: {
+//       database: "my_db",
+//       user: "username",
+//       password: "password"
+//     },
+//     pool: {
+//       min: 2,
+//       max: 10
+//     },
+//     migrations: {
+//       tableName: "knex_migrations"
+//     }
+//   }
+
+// };
+
+module.exports = {
   development: {
-    client: "sqlite3",
+    client: "postgresql",
     connection: {
-      filename: "./dev.sqlite3"
+      database: "cars_c6", 
+      user: "alfiahsanitaqwim", 
+      password: "root"
+      // filename: "./dev.sqlite3"
     }
   },
 
@@ -41,7 +86,4 @@ const config: { [key: string]: Knex.Config } = {
       tableName: "knex_migrations"
     }
   }
-
 };
-
-module.exports = config;
---
+++ src/middleware/handlerLogger.ts
@@ -0,0 +1,8 @@
+import { Request, Response, NextFunction } from 'express';
+
+const handleLogger = (req: Request, res: Response, next: NextFunction): void => {
+    console.log(`User is accessing by using user agent: ${req.get('User-Agent')}`);
+    next();
+};
+
+export = handleLogger;
