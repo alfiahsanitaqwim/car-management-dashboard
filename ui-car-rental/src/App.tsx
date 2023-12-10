746209d6b6167deb3d1771e0f5d8241e2341fe2d
alfiahsanitaqwim <127421190+alfiahsanitaqwim@users.noreply.github.com>
2023-12-10 20:28:59 +0700

    add list cars

--- ui-car-rental/src/App.tsx
+++ ui-car-rental/src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React from "react";
+
+interface PropsListCars {
+  id_cars: number;
+  car_name: string;
+  availability: boolean;
+  capacity: number;
+  image_url: string;
+  price: number;
+  description: string;
+  id_car_type: number;
+  id_car_brand: number;
+  id_customer: number;
+}
 
 function App() {
+  const [list, setList] = React.useState<any>([]);
+  React.useEffect(() => {
+    (async () => {
+      await axios
+        .get("http://localhost:3000/v1/cars")
+        .then(({ data }: any) => {
+          setList(data.data);
+        })
+        .catch((z: any) => {
+          console.error(z);
+        });
+    })();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {list?.map((dt: PropsListCars, index: number) => {
+        return (
+          <div
+            key={index}
+            style={{ backgroundColor: "lightgrey", padding: 10, margin: 10 }}
+          >
+            <div>{dt?.car_name}</div>
+            <div>{dt?.description}</div>
+          </div>
+        );
+      })}
     </div>
   );
 }
